109288d6911beeb3392151f3a2ce74f0afd1ef01
pukhraj saini <pukhraj.saini@appinventiv.com>
2024-01-14 23:45:52 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import "./App.css";
-import LoginForm from "./components/login/login-form";
-// import Post from "./components/use-state/post";
-// import Welcome from "./components/welcome";
 
-function App() {
-  // return <Welcome></Welcome>;
-  // return <Post />;
-  return <LoginForm />;
-}
+function App() {}
 
 export default App;
--- src/components/welcome.js
+++
@@ -1,9 +0,0 @@
-function Welcome() {
-  return (
-    <div className="welcome-container">
-      <h1 className="welcome-text">Welcome To React Hooks Session</h1>
-    </div>
-  );
-}
-
-export default Welcome;
